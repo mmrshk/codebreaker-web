7b73a2ce1193c1b4debe10135631e09ba6d48e97
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-01-22 16:08:23 +0200

    Fixed autoload.rb

--- autoload.rb
+++ autoload.rb
@@ -1,9 +1,7 @@
 require 'codebreaker'
 require 'erb'
 require 'i18n'
-require 'pry'
 
 require_relative 'lib/i18n_config'
-require_relative 'lib/models/guess'
 require_relative 'lib/models/renderer'
 require_relative 'lib/racker'
